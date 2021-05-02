263b769b616650a7cc8eedee680070c8ef86e487
Namocat <christiandhen@gmail.com>
2021-05-02 21:55:54 +0800

    code for the computer

---
+++ Computer.py
@@ -0,0 +1,16 @@
+import random
+
+CHOICES = 'rps'
+
+#player_choice
+
+
+#computer_choice
+#Need help in computer function
+def get_computer_choice():
+
+    computer_choice = random.randint(0, 2)
+    computer_choice = CHOICES[computer_choice]
+    return computer_choice
+
+#game_mechanics
