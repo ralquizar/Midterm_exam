4daa68744f858006319ab5c498168923ea8c5342
Raid <rjalquizar@gmail.com>
2021-05-02 21:00:14 +0800

    Added function for player choice

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -4,7 +4,15 @@ CHOICES = 'rps'
 
 #player_choice
 
+def get_player_choice():
 
+    player_choice = None
+    while player_choice is None:
+        player_choice = input('Choices: \n(R)ock \n(P)aper \n(S)cissors \n\nPick: ')
+        if player_choice.lower() not in CHOICES:
+            player_choice = None
+    return player_choice.lower()
+    
 #computer_choice
 
 
